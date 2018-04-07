13845fac02da9b9784f5e04173d149cfae6609b9
Conrado Mateu Gisbert <conradomateugisbert@gmail.com>
2018-04-07 13:16:57 +0200

    Added Array Support in DictDetector

--- JSON2SWIFT/DictDetector.swift
+++ JSON2SWIFT/DictDetector.swift
@@ -56,18 +56,28 @@ class DictDetector {
                     } else if(arr[current].contains("},")) {
                         let lastOpenBrace = openBraces.removeLast()
                         let key = value.components(separatedBy: ": ").first
-                        if(openBraces.isEmpty) {
-                            addToDictFrom(key: key!, array: arr, indexes: lastOpenBrace+1, current-1)
-                        }
-                        if(current+1<arr.count) {
-                            print(current)
-                            indexDetector(arr: arr, index: current+1, isFirst: false)
-                        }
+                        if(openBraces.isEmpty) {addToDictFrom(key: key!, array: arr, indexes: lastOpenBrace+1, current-1)}
+                        if(current+1<arr.count) {indexDetector(arr: arr, index: current+1, isFirst: false)}
                     }
                 }
             }
-        } else {
-                addToDictFrom(array: arr, index: index)
+        } else if(value.contains(": [")){
+            var current = index
+            var found = false
+            var opener = -1
+            while !found{
+                if(arr[current].contains("{")){opener = current
+                }else if(arr[current].contains("},")){
+                    let key = value.components(separatedBy: ": ").first
+                    found = true
+                    addToDictFrom(key: key!, array: arr, indexes: opener+1, current-1)
+                }
+                current += 1
+            }
+            while !arr[current].contains("]"){current += 1}
+            if(current+1<arr.count) {indexDetector(arr: arr, index: current+1, isFirst: false)}
+        }else{
+            addToDictFrom(array: arr, index: index)
             if(index+1<arr.count) {indexDetector(arr: arr, index: index+1, isFirst: false)}
             }
     }
