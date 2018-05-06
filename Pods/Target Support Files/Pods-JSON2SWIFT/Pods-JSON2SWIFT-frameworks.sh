0e2ff05b40d70093828ebb4c5cf3d3207811bf66
Conrado Mateu Gisbert <conradomateugisbert@gmail.com>
2018-05-06 22:24:43 +0200

    Added UI Buttons

--- Pods/Target Support Files/Pods-JSON2SWIFT/Pods-JSON2SWIFT-frameworks.sh
+++ Pods/Target Support Files/Pods-JSON2SWIFT/Pods-JSON2SWIFT-frameworks.sh
@@ -91,11 +91,15 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Bond/Bond.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Differ/Differ.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/FlatButton/FlatButton.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Highlightr/Highlightr.framework"
   install_framework "$BUILT_PRODUCTS_DIR/ReactiveKit/ReactiveKit.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Bond/Bond.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Differ/Differ.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/FlatButton/FlatButton.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Highlightr/Highlightr.framework"
   install_framework "$BUILT_PRODUCTS_DIR/ReactiveKit/ReactiveKit.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
